1690daff6f5782f787375441ddcf643a79a201c5
proto0654 <34603794+proto0654@users.noreply.github.com>
2023-12-29 20:32:24 +0300

    Update some.js

--- some.js
+++ some.js
@@ -1,1 +1,15 @@
-console.log('snippet executed');
+function insertAtCursor(text) {
+    var input = document.activeElement;
+    if (input.selectionStart || input.selectionStart == '0') {
+        var startPos = input.selectionStart;
+        var endPos = input.selectionEnd;
+        input.value = input.value.substring(0, startPos)
+            + text
+            + input.value.substring(endPos, input.value.length);
+    } else {
+        input.value += text;
+    }
+}
+
+// Пример использования:
+insertAtCursor('Вставляемый текст');
